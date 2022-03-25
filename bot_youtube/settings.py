de0b75ff25b5cda4477cf16b94a39a22ac3bb59a
Luthfi A. Ardhani <luthfidhani17@gmail.com>
2022-03-25 23:02:38 +0700

    remove django heroku from settings

--- bot_youtube/settings.py
+++ bot_youtube/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 from pathlib import Path
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
